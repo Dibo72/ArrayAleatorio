073cd384600789fd20e5dee1bc035c86acd6a306
Dibo72 <humanida133@gmail.com>
2026-02-24 12:43:20 +0100

    Ejercicio acabado

--- src/ArrayAleatorio.java
+++ src/ArrayAleatorio.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+public class ArrayAleatorio {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int media = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Digite um numero: ");
+        int longitud = sc.nextInt();
+        int[] aleatorio = new int[longitud];
+
+        for (int i = 0; i < longitud; i++) {
+            aleatorio[i] = (int)(Math.random()*100);
+        }
+
+        for (int i = 0; i < longitud; i++) {
+            System.out.print(aleatorio[i] + " ");
+        }
+
+        for (int i = 0; i < longitud; i++) {
+            media += aleatorio[i];
+        }
+        System.out.println();
+        media=media/longitud;
+        System.out.println("Media: " + media);
+
+        System.out.println();
+
+        System.out.println("Mayores que la media");
+        for (int i = 0; i < longitud; i++) {
+            if(aleatorio[i]>media){
+                System.out.print(aleatorio[i] + " ");
+            }
         }
     }
 }
